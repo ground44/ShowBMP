52d9e9aa9d22073582716179322fafc24a5f78cf
gr0und <unknown>
2017-07-15 11:08:40 -0400

    added .gitignore

---
+++ README.md
@@ -0,0 +1,3 @@
+A small tutorial on how to load a .bmp file with openGL.
+
+goes together with this article: <<COMING>>
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+
+void main(int argc,char *argv[]){
+	if(argc<2){
+		cout<<"No file given"<<endl;
+	}
+}
